cdb45ffc1e96d157c76845ec71739ff1f97d1f48
Rahul Shridhar <rahulshridhar@Rahuls-MacBook-Pro.local>
2020-11-17 00:39:35 -0800

    Add helper function to draw pixels on the board

--- Pixel.h
+++ Pixel.h
@@ -6,6 +6,8 @@
 class Pixel : public QGraphicsPixmapItem {
 public:
     Pixel(QGraphicsPixmapItem *parent) : pixmap(parent) {}
+    void set_Pixmap(QPixmap image) { pixmap->setPixmap(image); }
+    void set_Pixmap(QGraphicsPixmapItem *image) {pixmap->setPixmap(image->pixmap());}
 private:
     QGraphicsPixmapItem *pixmap;
 };
