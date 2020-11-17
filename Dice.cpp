da4ee5afb26cca81ba3a04c0c22d4a4144201ce0
Rahul Shridhar <rahulshridhar@Rahuls-MacBook-Pro.local>
2020-11-17 00:40:08 -0800

    Add uniform distribution for die roll

--- Dice.cpp
+++ Dice.cpp
@@ -1,7 +1,10 @@
 #include "Dice.h"
-
+#include <random>
 
 int Dice::roll()
 {
-
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> distrib(1, 6);
+    return distrib(gen);
 }
