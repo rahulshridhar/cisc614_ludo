8fe665af7eef047004a803cbbc7492646c26fca3
Rahul Shridhar <rahulshridhar@Rahuls-MacBook-Pro.local>
2020-11-20 00:55:05 -0800

    add pawn on a specific field; set and getters for this pawn

--- Pixel.h
+++ Pixel.h
@@ -3,12 +3,17 @@
 
 #include <QGraphicsPixmapItem>
 
+class Pawn;
+
 class Pixel : public QGraphicsPixmapItem {
 public:
     Pixel(QGraphicsPixmapItem *parent) : pixmap(parent) {}
     void set_Pixmap(QPixmap image) { pixmap->setPixmap(image); }
     void set_Pixmap(QGraphicsPixmapItem *image) {pixmap->setPixmap(image->pixmap());}
+    void set_pawn(Pawn *new_pawn) { curr_pawn = new_pawn; }
+    Pawn* get_pawn() { return curr_pawn; }
 private:
+    Pawn* curr_pawn = nullptr;
     QGraphicsPixmapItem *pixmap;
 };
 
