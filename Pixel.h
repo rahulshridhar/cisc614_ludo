02b5859c9811f10fa41c7a769786f31c05d5e88e
Rahul Shridhar <rahulshridhar@Rahuls-MacBook-Pro.local>
2020-11-25 00:09:45 -0800

    Add support for multiple pawns on a pixel; needs some fixes

--- Pixel.h
+++ Pixel.h
@@ -10,10 +10,14 @@ public:
     Pixel(QGraphicsPixmapItem *parent) : pixmap(parent) {}
     void set_Pixmap(QPixmap image) { pixmap->setPixmap(image); }
     void set_Pixmap(QGraphicsPixmapItem *image) {pixmap->setPixmap(image->pixmap());}
-    void set_pawn(Pawn *new_pawn) { curr_pawn = new_pawn; }
-    Pawn* get_pawn() { return curr_pawn; }
+    QGraphicsPixmapItem * getPixmap() { return pixmap; }
+    void set_pawn(Pawn *new_pawn) { curr_pawns.push_back(new_pawn); }
+    Pawn* get_pawn() { return curr_pawns.size() ? curr_pawns[0] : nullptr; }
+    void remove_pawn() { curr_pawns.erase(curr_pawns.begin()); }
+    bool is_empty() { return curr_pawns.size() == 0; }
+    bool has_more_than_one_pawn() { return curr_pawns.size() > 1; }
 private:
-    Pawn* curr_pawn = nullptr;
+    std::vector<Pawn*> curr_pawns = {};
     QGraphicsPixmapItem *pixmap;
 };
 
