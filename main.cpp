b4d54ac880925d60c0e3f22298e3cde3271f656c
Rahul Shridhar <rahulshridhar@Rahuls-MacBook-Pro.local>
2020-12-02 00:15:49 -0800

    Add ability to run multiple games and compute their stats; moved view inside Board object

--- main.cpp
+++ main.cpp
@@ -1,21 +1,41 @@
 #include <QApplication>
-#include <QGraphicsView>
 #include "Board.h"
+#include <iostream>
 
+int total_games = 1;
+int blue_wins = 0;
+int red_wins = 0;
+int yellow_wins = 0;
+int green_wins = 0;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    for (int i = 0; i < total_games; i++) {
+        QApplication a(argc, argv);
 
-    Board *scene = new Board();
+        Board *board = new Board();
 
-    QGraphicsView *view = new QGraphicsView(scene);
-    view->show();
-    view->setFixedSize(1000, 800);
+        board->setSceneRect(0, 0, 1000, 900);
+        board->draw();
+        board->play();
 
-    scene->setSceneRect(0, 0, 1000, 900);
-    scene->draw();
-    scene->play();
+        //QObject::connect(scene, &Board::exit, &a, &QApplication::quit, Qt::QueuedConnection);
+        //if (scene->play()) emit scene->exit();
 
-    return a.exec();
+        a.exec();
+        //blue = 0, red = 1, yellow = 2, green = 3
+        if (board->get_turn() == 0) blue_wins++;
+        else if (board->get_turn() == 1) red_wins++;
+        else if (board->get_turn() == 2) yellow_wins++;
+        else if (board->get_turn() == 3) green_wins++;
+
+        delete board;
+    }
+
+    std::cout << "Total games = " << total_games << std::endl;
+    std::cout << "Blue wins = " << blue_wins << std::endl;
+    std::cout << "Red wins = " << red_wins << std::endl;
+    std::cout << "Yellow wins = " << yellow_wins << std::endl;
+    std::cout << "Green wins = " << green_wins << std::endl;
+    return 0;
 }
