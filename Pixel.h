6311ac8f429bcc262ad698ef9c32dff398ce838c
Rahul Shridhar <rahulshridhar@Rahuls-MacBook-Pro.local>
2020-12-05 23:50:13 -0800

    Fix function to remove pawn from pixel; more helper functions to get all pawns on a pixel

--- Pixel.h
+++ Pixel.h
@@ -13,9 +13,11 @@ public:
     QGraphicsPixmapItem * getPixmap() { return pixmap; }
     void set_pawn(Pawn *new_pawn) { curr_pawns.push_back(new_pawn); }
     Pawn* get_pawn() { return curr_pawns.size() ? curr_pawns[0] : nullptr; }
-    void remove_pawn() { curr_pawns.erase(curr_pawns.begin()); }
+    void remove_pawn(Pawn* pawn) { curr_pawns.erase(std::remove(curr_pawns.begin(), curr_pawns.end(), pawn), curr_pawns.end()); }
     bool is_empty() { return curr_pawns.size() == 0; }
     bool has_more_than_one_pawn() { return curr_pawns.size() > 1; }
+    int get_num_pawns() { return curr_pawns.size(); }
+    std::vector<Pawn*> get_curr_pawns() { return curr_pawns; }
 private:
     std::vector<Pawn*> curr_pawns = {};
     QGraphicsPixmapItem *pixmap;
